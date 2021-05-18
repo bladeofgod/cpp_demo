79e0724a45d4760ad79d6f0d7800950314c8967b
李佳奇 <450103187@qq.com>
2021-05-18 16:25:14 +0800

    day2_pi_demo

---
+++ day2/request_pi.h
@@ -0,0 +1,36 @@
+//
+// Created by Admin on 2021/5/18.
+//
+
+#include "iostream"
+
+using namespace std;
+
+double arcTan(double x);
+
+#ifndef CPPDEMO_REQUEST_PI_H
+#define CPPDEMO_REQUEST_PI_H
+
+void runPiDemo(){
+    double a = 16.0 * arcTan(1.0/5.0);
+    double b = 4.0 * arcTan(1.0/239.0);
+    double pi = a - b;
+    cout<<pi<<endl;
+}
+
+double arcTan(double x){
+    double head = x;
+    int tail = 1;
+    double art = 0;
+    while (double(head/tail) > 1e-15) {
+        art = (tail%4 == 1) ? art + head/tail : art - head/tail;
+        head *= x*x;
+        tail +=2;
+        cout<<"---------------"<<endl;
+        cout<<tail<<endl;
+        cout<<"---------------"<<endl;
+    }
+}
+
+
+#endif //CPPDEMO_REQUEST_PI_H
